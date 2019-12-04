97c3606d1773826442876e43d57861384d282ae4
Jovan Popovic <jocapc@gmail.com>
2019-12-04 21:55:39 +0100

    Adding more reports

--- sql/queries-top-cpu.sql
+++ sql/queries-top-cpu.sql
@@ -1,4 +1,4 @@
-SELECT query_id, [Duration(sec)] = CAST(duration_s AS NUMERIC(8,1)), count_executions, [CPU(sec)] = CAST(cpu_time_ms AS NUMERIC(8,1)),  text
+SELECT TOP 10 query_id, [Duration(sec)] = CAST(duration_s AS NUMERIC(8,1)), count_executions, [CPU(sec)] = CAST(cpu_time_ms AS NUMERIC(8,1))/1000,  text
 FROM qpi.db_query_exec_stats_history
 order by cpu_time_ms desc
 
--- sql/queries-top-tempdb.sql
+++ sql/queries-top-tempdb.sql
@@ -1,4 +1,4 @@
-SELECT query_id, [TempDB used(MB)] = tempdb_used_mb,
+SELECT TOP 10 query_id, [TempDB used(MB)] = tempdb_used_mb,
 		[Duration(sec)] = CAST(duration_s AS NUMERIC(8,1)), count_executions, [CPU(sec)] = CAST(cpu_time_ms AS NUMERIC(8,1))/1000,
 		text
 FROM qpi.db_query_exec_stats_history
