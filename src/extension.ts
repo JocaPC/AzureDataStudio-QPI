2e3a86011468629d36b3d7e5c62b4b758233e1c3
Jovan Popovic <jocapc@gmail.com>
2020-03-07 20:08:46 +0100

    Added fully functional code

--- src/extension.ts
+++ src/extension.ts
@@ -1,13 +1,24 @@
 'use strict';
 import * as vscode from 'vscode';
 import * as request from 'request-promise-native';
-import * as sqlops from 'azdata';
-import {placeScript} from './placescript';
+
+async function showScript(fileName, scriptText) {
+    var setting: vscode.Uri = vscode.Uri.parse("untitled:" + fileName);
+    try {
+        let doc = await vscode.workspace.openTextDocument(setting);
+        let editor = await vscode.window.showTextDocument(doc, 1, false);
+        editor.edit(edit => {
+            edit.insert(new vscode.Position(0, 0), scriptText);
+        });
+        vscode.window.showInformationMessage("Execute the script on the target database.");
+    } catch (err) {
+        vscode.window.showErrorMessage(err);
+    }
+}
 
 export function activate(context: vscode.ExtensionContext) {
     const baseUrl = "https://raw.githubusercontent.com/jocapc/QPI/master/src/";
 
-    
     // Importing QPI scripts
     var installQPI = async () => {
         let fileName = "qpi.sql";
@@ -15,7 +26,7 @@ export function activate(context: vscode.ExtensionContext) {
             uri: baseUrl + fileName,
         };
         const scriptText = await request.get(options);
-        new placeScript().placescript(fileName,scriptText);
+        await showScript(fileName, scriptText);
     };
     var installQpiCmd = vscode.commands.registerCommand('qpi.install', installQPI);
     context.subscriptions.push(installQpiCmd);
@@ -27,7 +38,7 @@ export function activate(context: vscode.ExtensionContext) {
             uri: baseUrl + fileName,
         };
         const scriptText = await request.get(options);
-        new placeScript().placescript(fileName,scriptText);
+        await showScript(fileName,scriptText);
     };
     var removeQpiCmd = vscode.commands.registerCommand('qpi.remove', removeQPI);
     context.subscriptions.push(removeQpiCmd);
@@ -39,11 +50,10 @@ export function activate(context: vscode.ExtensionContext) {
             uri: baseUrl + fileName,
         };
         const scriptText = await request.get(options);
-        new placeScript().placescript(fileName,scriptText);
+        await showScript(fileName,scriptText);
     };
     var installQpiAgentCmd = vscode.commands.registerCommand('qpi.schedule', installQPIAgent);
     context.subscriptions.push(installQpiAgentCmd);
-
 }
 
 export function deactivate() {
